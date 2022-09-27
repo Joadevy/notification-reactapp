b95835189f7bbddab40b98bf19cc2f114fc91cb5
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-27 18:25:16 -0300

    Working w/ notif component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-// import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Notification from "./Notification.jsx";
 
-//  useEffect(() => {
-//    requestPets();
-//  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
 function App() {
-  let NOTIFICATIONS = [1, 2, 3, 4];
+  // let NOTIFICATIONS = [1, 2, 3, 4];
+  const [NOTIFICATIONS, setNOTIFICATIONS] = useState([]);
+  // let NOTIFICATIONS = [];
 
   useEffect(() => {
     requestData();
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestData() {
     const data = await fetch("./data.json");
     const res = await data.json();
-    NOTIFICATIONS = res.notification;
+    setNOTIFICATIONS(res.notification);
     console.log(NOTIFICATIONS);
   }
 
   return (
-    <main className="font-jakarta max-w-3xl bg-neutral-white border-2 border-black rounded-md p-6">
-      <header className="text-neutral-dark-blue flex justify-between gap-72">
+    <main className="font-jakarta max-w-3xl bg-neutral-white shadow-xl rounded-md p-8 m-4">
+      <header className="text-neutral-dark-blue flex justify-between gap-72 mt-5 mb-10">
         <div className="flex justify-center items-center gap-4">
           <h1 className="font-bold text-2xl">Notifications</h1>
           <div className="w-10 h-8 bg-primary-blue rounded-lg flex items-center justify-center">
             <p className="text-neutral-50 font-bold">3</p>
           </div>
         </div>
-        <button onClick={() => console.log("hola")}>Mark all as read</button>
+        <button
+          onClick={() => console.log("hola")}
+          className="text-neutral-500 font-semibold"
+        >
+          Mark all as read
+        </button>
       </header>
-      {NOTIFICATIONS.map((notif) => (
-        <Notification key={notif} />
-      ))}
+      <div className="flex flex-col gap-3">
+        {NOTIFICATIONS.map((notif, index) => (
+          <Notification key={index} props={notif} />
+        ))}
+      </div>
     </main>
   );
 }
--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,5 +1,30 @@
-const Notification = () => {
-  return <div>hello world, im a component!</div>;
+const Notification = ({ props }) => {
+  const { image, name, data, time, type, content } = props;
+  // Should have a state with an active/disable notification and
+  // a hover with cursor pointer if the notification is active to disable it.
+  return (
+    <div className="flex gap-2 p-4 w-full rounded-lg border-2 border-transparent hover:border-2 hover:border-neutral-200">
+      <div className="w-[6%]">
+        <img src={image} alt={name} className="w-full" />
+      </div>
+      <div className="flex flex-col leading-none gap-2 w-11/12">
+        <p className="text-neutral-600">
+          <span className="font-bold hover:text-primary-blue">{name}</span>{" "}
+          <span className="text-neutral-500">{data}</span>{" "}
+          {type === "group" ? (
+            <span className="font-bold hover:text-neutral-600 text-primary-blue">
+              {content}
+            </span>
+          ) : type === "reaction" ? (
+            <span className="font-bold text-neutral-600">{content}</span>
+          ) : (
+            ""
+          )}
+        </p>
+        <p className="text-xs text-neutral-400">{time}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
